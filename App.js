01afbc6ddb96294890d5d8ba3213d431c5e702ee
Rodrigo Bossini <professorbossini@gmail.com>
2022-11-05 12:50:49 -0300

    apostila05(react-native): fim da apostila

--- App.js
+++ App.js
@@ -1,20 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  Button,
+  FlatList,
+  StyleSheet, 
+  Text, 
+  TextInput, 
+  View 
+} from 'react-native';
 
-export default function App() {
+// hooks do React (a partir da versão 16.8)
+import { useState } from 'react';
+
+// single source of truth SSOT
+export default function App(){
+  const [todoItem, setTodoItem] = useState('')
+  const [todoItens, setTodoItens] = useState([])
+
+  const capturarTodoItem = (todoItemDigitado) => {
+    // todoItem = todoItemDigitado
+    setTodoItem(todoItemDigitado)
+  }
+
+  const adicionarTodoItem = () => {
+    setTodoItens(todoItensAtual => {
+      const todoItensNovo = [todoItem, ...todoItensAtual]
+      setTodoItem('')
+      return todoItensNovo
+    })      
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{padding: 40}}>
+      <View style={{marginBottom: 4}}>
+        <TextInput 
+          style={estilos.todoTextInput}
+          onChangeText={capturarTodoItem}
+          value={todoItem}
+        />
+        <Button 
+          title='OK'
+          onPress={adicionarTodoItem}
+        />
+      </View>
+
+      <View>
+        <FlatList 
+          data={todoItens}
+          renderItem={({item}) =>(
+            <View style={estilos.todoItemNaLista}>
+              <Text>{item}</Text>
+            </View>
+          )}
+        />
+
+      </View>
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const estilos = StyleSheet.create({
+  todoTextInput: {
+    borderBottomColor: '#CCC',
+    borderBottomWidth: 1,
+    padding: 12,
+    marginBottom: 4 
   },
-});
+  todoItemNaLista: {
+    backgroundColor: '#CCC',
+    padding: 12,
+    borderColor: '#FFF',
+    borderWidth: 1,
+    marginBottom: 4 ,
+    borderRadius: 8 
+  }  
+})
+
+
+        {/* {
+          todoItens.map(todoItem => (
+          <View style={estilos.todoItemNaLista}>
+            <Text>{todoItem}</Text>
+          </View>
+          ))
+        } */}
